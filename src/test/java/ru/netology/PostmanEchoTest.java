d109d8835c776bd30077775a99d79d5d78a1f170
TaisiaKos <tasia.pcma.le4@mail.ru>
2023-12-28 20:29:04 +0300

    fail

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -1,8 +1,9 @@
 package ru.netology;
 
 import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
 
@@ -12,14 +13,14 @@ public class PostmanEchoTest {
 // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("Привет, мир!") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("Hello, world!") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Привет, мир!"));
+                .body("data", equalTo("Hi"));
     }
 
 
